520c594d08be6f5bd288a2908f393b935a831e18
unknown <Stefan.marinkovic@botkyrka.skola.se>
2020-09-03 10:06:24 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(8, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(8, HIGH);   
+  delay(300);              
+  digitalWrite(8, LOW);   
+  delay(5000);  
+  // put your main code here, to run repeatedly:
+
+}
